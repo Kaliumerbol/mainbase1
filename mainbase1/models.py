be29f87a17e5f9e890a718518f0dad7009efb453
Kaliumerbol <kaliumerbol@gmail.com>
2021-11-07 23:54:00 +0600

    Add task model

--- mainbase1/models.py
+++ mainbase1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import  get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Mainbase1(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mainbase1', null=True)
+    reg_id = models.IntegerField(auto_created=True)
+    register_at = models.DateTimeField(auto_now_add=True)
+    pr_name = models.TextField()
+    pr_zacaz = models.TextField()
+    pr_proectir = models.TextField()
+    finished_at = models.DateTimeField(null=True)
+    is_finished = models.BooleanField(default=False)
+    estimated_finish_time = models.DateTimeField()
